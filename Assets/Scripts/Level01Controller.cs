5fb64ed8cf81e0a96b0dd490d67d8a15eb5742f7
Jessica Macaspac <jeccamac@gmail.com>
2022-02-19 16:55:07 -0600

    Menu and Buttons
    Main menu creation and MainMenu and Level01 creation. Start and Credits button linked and looped

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level01Controller : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+
+    public void ExitLevel()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
