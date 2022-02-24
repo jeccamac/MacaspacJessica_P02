6b56ffb95f09360fdf4261884d0011276d27b298
Jessica Macaspac <jeccamac@gmail.com>
2022-02-23 19:19:10 -0600

    Audio
    Added audio manager and background music to main menu and persist throughout game

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+
+    void Start()
+    {
+        if (_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+
+}
