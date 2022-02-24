1ea2e0b9dfb4b8098ec4f650b41442d9c0eb8f8f
Jessica Macaspac <jeccamac@gmail.com>
2022-02-23 19:55:59 -0600

    Project Progress Week 1
    Set up score and saving/loading high score. Finish up this week's progress

--- Assets/Scripts/Level01Controller.cs
+++ Assets/Scripts/Level01Controller.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Level01Controller : MonoBehaviour
 {
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
     private void Update()
     {
+        // Increase Score manually
+        // TODO replace with real implementation later
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+        // Exit Level
+        // TODO bring up popup menu for navigation
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ExitLevel();
@@ -15,6 +27,24 @@ public class Level01Controller : MonoBehaviour
 
     public void ExitLevel()
     {
+        // compare score to high score
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            // save current score as new high score
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+
+        // load new level
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        // increase score
+        _currentScore += scoreIncrease;
+        // update score display so we can see the new score
+        _currentScoreTextView.text = "Score: " + _currentScore.ToString();
     }
 }
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView;
 
     void Start()
     {
+        // load high score display
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+
+        // play starting song on Menu Start
         if (_startingSong != null)
         {
             AudioManager.Instance.PlaySong(_startingSong);
