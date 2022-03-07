b07510a179a47ac58606c5a148925323b993f2d1
Jessica Macaspac <jeccamac@gmail.com>
2022-03-06 21:07:47 -0600

    Basic FPS player movement
    Character controller with camera attached to head, WASD movement and rotation with the mouse, gravity and jump implemented

---
+++ Assets/Scripts/Examples/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 100;
+
+    UIManager uiManager;
+    private void Awake()
+    {
+        // fill our references
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DamagePlayer(10);
+        }
+    }
+
+    public void DamagePlayer(int damageAmount)
+    {
+        // subtract from health
+        health -= damageAmount;
+        Debug.Log("Health is now " + health);
+
+        // update the slider
+        uiManager.UpdateHealth();
+
+    }
+}
